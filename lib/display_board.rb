2a3da788654ed6da3500d7f114b7a1c1f11f8982
Kathryn Senack <katbrooke0219@gmail.com>
2021-06-06 21:22:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 def display_board(board)
-  b = " " + board[0] + " | " + board[1] + " | " + board[2] + " "
-  b = b + " " + board[3] + " | " + board[4] + " | " + board[5] + " "
-  b = b + " " + board[3] + " | " + board[4] + " | " + board[5] + " "
+  b = " " + board[0] + " | " + board[1] + " | " + board[2] + " " + "\n"
+  b = b + " " + board[3] + " | " + board[4] + " | " + board[5] + " " + "\n"
+  b = b + " " + board[6] + " | " + board[7] + " | " + board[8] + " " + "\n"
+  puts b
 end
 
