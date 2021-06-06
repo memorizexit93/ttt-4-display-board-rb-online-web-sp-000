83204bb1b90e715031cd0dabcef9930a439a4914
Kathryn Senack <katbrooke0219@gmail.com>
2021-06-06 21:18:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
 def display_board(board_info)
-  " " + board_info[0] + " | " + board_info[1] + " | " + board_info[2] + " "
+  board = " " + board_info[0] + " | " + board_info[1] + " | " + board_info[2] + " "
+  board = board + " " + board_info[3] + " | " + board_info[4] + " | " + board_info[5] + " "
 end
