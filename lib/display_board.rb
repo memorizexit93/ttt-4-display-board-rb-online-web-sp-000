576e7be80f1260da06670f6df2b145bc73cf38d0
Kathryn Senack <katbrooke0219@gmail.com>
2021-06-06 21:38:47 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  b = " " + board[0] + " | " + board[1] + " | " + board[2] + " " + "\n"
+  b += "-----------" +"\n"
+  b += " " + board[3] + " | " + board[4] + " | " + board[5] + " " + "\n"
+  b += "-----------" + "\n"
+  b += " " + board[6] + " | " + board[7] + " | " + board[8] + " " + "\n"
+  print b
+end
+
